779a49be22bb67f5ea0bb4c9f17e3d0cfc8c56a6
tarunchaudharydev <tarunchaudhary.dev@gmail.com>
2025-12-28 13:01:15 +0530

    update the documentation

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: "Techtronica",
-  description: "Created by Techtronica",
+  description: "Made with ❤️ by Techtronica",
 };
 
 export default function RootLayout({
