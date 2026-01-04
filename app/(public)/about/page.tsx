1cc69094136c2aeb3b467e1ffda2f31d5b2a6efc
tarunchaudharydev <tarunchaudhary.dev@gmail.com>
2026-01-04 17:17:19 +0530

    update top of about us section

--- app/(public)/about/page.tsx
+++ app/(public)/about/page.tsx
@@ -12,7 +12,7 @@ export default function AboutPage() {
   return (
     <main className="bg-gray-50 text-gray-700 min-h-screen">
       {/* HERO SECTION */}
-      <section className="relative h-[70vh] -mt-20">
+      <section className="relative h-[70vh] -mt-32">
         <Image
           src="/ourTeam/about.jpg"
           alt="Technology background"
